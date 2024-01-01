d505e7f56dbaca6e7133aa1365a74c6fbce82b48
Achladi-Anudeep <achladi.sai.anudeep@gmail.com>
2024-01-01 09:28:23 +0530

    mern-auth frontend boilerplate deployment #4

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "" });
+const baseQuery = fetchBaseQuery({
+  baseUrl: "https://mern-auth-boilerplate-api.onrender.com",
+});
 export const apiSlice = createApi({
   baseQuery,
   tagTypes: ["User"],
